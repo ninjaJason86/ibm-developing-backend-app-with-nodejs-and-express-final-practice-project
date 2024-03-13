cf33d1a3e7bde20dd9168a70669e75c92c9623db
ninjaJason86 <jsntrout@gmail.com>
2024-03-13 15:46:40 +0200

    router: friends: format file

--- router/friends.js
+++ router/friends.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 let friends = {
-    "johnsmith@gamil.com": {"firstName": "John","lastName": "Doe","DOB":"22-12-1990"},
-    "annasmith@gamil.com":{"firstName": "Anna","lastName": "smith","DOB":"02-07-1983"},
-    "peterjones@gamil.com":{"firstName": "Peter","lastName": "Jones","DOB":"21-03-1989"}
+  "johnsmith@gamil.com": { "firstName": "John", "lastName": "Doe", "DOB": "22-12-1990" },
+  "annasmith@gamil.com": { "firstName": "Anna", "lastName": "smith", "DOB": "02-07-1983" },
+  "peterjones@gamil.com": { "firstName": "Peter", "lastName": "Jones", "DOB": "21-03-1989" }
 };
 
 
 // GET request: Retrieve all friends
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
 
   // Update the code here
 
@@ -18,14 +18,14 @@ router.get("/",(req,res)=>{
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
-router.get("/:email",(req,res)=>{
+router.get("/:email", (req, res) => {
   // Update the code here
   res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
 
 // POST request: Add a new friend
-router.post("/",(req,res)=>{
+router.post("/", (req, res) => {
   // Update the code here
   res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
@@ -44,4 +44,4 @@ router.delete("/:email", (req, res) => {
   res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
-module.exports=router;
+module.exports = router;
